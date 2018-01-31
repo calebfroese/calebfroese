85d00aeaf23c3a3351667e3bdd54a2ac18e8a84c
Caleb Froese <caleb.froese@gmail.com>
2018-01-31 21:54:26 +1030

    Add placeholder navigation dots

--- src/components/page-navigation/page-navigation.component.ts
+++ src/components/page-navigation/page-navigation.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-nav',
-  templateUrl: './page-nav.component.html',
-  styleUrls: ['./page-nav.component.scss'],
+  templateUrl: './page-navigation.component.html',
+  styleUrls: ['./page-navigation.component.scss'],
 })
 export class PageNavigationComponent {}
